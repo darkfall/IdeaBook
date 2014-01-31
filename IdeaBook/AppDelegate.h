88344425b7728ec972287f9bab87d34d87d01fd1
darkfall <darkfall@gamemastercn.com>
2014-01-30 23:09:50 -0500

    Initial Commit

---
+++ IdeaBook/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IdeaBook
+//
+//  Created by Robert Bu on 1/30/14.
+//  Copyright (c) 2014 Robert Bu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IdeaBook/SecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  IdeaBook
+//
+//  Created by Robert Bu on 1/30/14.
+//  Copyright (c) 2014 Robert Bu. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
